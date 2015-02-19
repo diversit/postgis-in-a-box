107faa4c915f57a7af0b80d490de6f56dc0c9d3d
Joost den Boer <jdboer@diversit.eu>
2015-02-19 08:04:34 +0100

    Some last changes to get postgis-in-a-box completely working correctly.

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "precise64"
+  config.vm.box = "hashicorp/precise64"
 #  config.vm.box = "precise64-current"
 #  config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/precise/current/precise-server-cloudimg-amd64-vagrant-disk1.box"
 
--- post_vm_install.sh
+++ post_vm_install.sh
@@ -6,7 +6,7 @@ wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 
 sudo apt-get update
-sudo apt-get install python-dev
+sudo apt-get install -y python-dev
 
 sudo pip install ansible
 
@@ -14,22 +14,24 @@ echo "Cleaning up apt..."
 sudo apt-get clean
 sudo apt-get autoclean
 
-echo "Setting postgres password"
-sudo su - postgres psql -c "ALTER USER postgres PASSWORD 'secretpassword';"
-
 echo "Running Ansible Playbook"
-ansible-playbook /vagrant/provisioning/playbook.yml --connection=local
+ansible-playbook /vagrant/provisioning/playbook.yml -i /vagrant/provisioning/hosts-vagrant --connection=local
+
+echo "Setting postgres password"
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'secretpassword';"
 
 echo "Creating uselessapp database"
-sudo su - postgres psql -c "CREATE DATABASE uselessapp;"
+sudo -u postgres psql -c "CREATE DATABASE uselessapp;"
 
 echo "Installing PostGIS extension in uselessapp db"
-sudo su - postgres psql -d uselessapp -c "CREATE EXTENSION postgis;"
-sudo su - postgres psql -d uselessapp -c "CREATE EXTENSION postgis_topology;"
-sudo su - postgres psql -d uselessapp -c "CREATE EXTENSION postgis_tiger_geocoder;"
+sudo -u postgres psql -d uselessapp -c "CREATE EXTENSION postgis;"
+sudo -u postgres psql -d uselessapp -c "CREATE EXTENSION postgis_topology;"
+sudo -u postgres psql -d uselessapp -c "CREATE EXTENSION postgis_tiger_geocoder;"
 
 echo "Loading GIS data in uselessapp db"
-sudo su - postgres psql -d uselessapp -f /vagrant/gis-data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.sql
-sudo su - postgres psql -d uselessapp -f /vagrant/gis-data/ne_10m_admin_0_sovereignty/ne_10m_admin_0_sovereignty.sql
+#sudo -u postgres psql -d uselessapp -f /vagrant/gis-data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.sql
+#sudo -u postgres psql -d uselessapp -f /vagrant/gis-data/ne_10m_admin_0_sovereignty/ne_10m_admin_0_sovereignty.sql
+sudo -u postgres shp2pgsql -I -s 2249 /vagrant/gis-data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp public.geo_countries | sudo -u postgres psql -d uselessapp 
+sudo -u postgres shp2pgsql -I -s 2249 /vagrant/gis-data/ne_10m_admin_0_sovereignty/ne_10m_admin_0_sovereignty.shp public.geo_sovereignty | sudo -u postgres psql -d uselessapp 
 
 echo "Done!"
